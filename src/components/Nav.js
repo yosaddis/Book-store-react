8f53f0509a59bc168921056435810c0a08f160fc
yosaddis <60548252+yosaddis@users.noreply.github.com>
2023-04-22 14:40:11 +0300

    Apply new styling to components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,48 +1,27 @@
 import React from 'react';
-import { Link as ReachRouterLink } from 'react-router-dom';
-import styled from 'styled-components';
-
-const Header = styled.div`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  border-bottom: solid #e0e0e0 1px;
-  margin-bottom: 3rem;
-`;
-
-const Title = styled.h1`
-  color: #f5913e;
-`;
-
-const Menu = styled(ReachRouterLink)`
-  background-color:  #f5913e;
-  margin: 1rem;
-  height: fit-content;
-  color:#fff;
-  border: 0;
-  font-size: 1.2em;
-  border-radius: 3px;
-  padding: 8px 17px;
-  cursor: pointer;
-  text-decoration: none;
-  box-sizing: border-box;
-
-  &:hover {
-    background: #fff;
-    color: #f5913e;
-    border: 1px solid #f5913e;
-  }
-`;
+import '../style/style.css';
+import { NavLink } from 'react-router-dom';
+import { BsFillPersonFill } from 'react-icons/bs';
 
 function Nav() {
   return (
-    <Header>
-      <Title>Book Store</Title>
-      <nav>
-        <Menu to="/">Books</Menu>
-        <Menu to="/Categories">Categories</Menu>
-      </nav>
-    </Header>
+    <nav className="navbar">
+      <h1 className="Bookstore-CMS">Bookstore CMS</h1>
+      <ul className="menu-items">
+        <li>
+          <NavLink className="menu-items" to="/">BOOKS</NavLink>
+        </li>
+        <li>
+          <NavLink className="menu-items" to="/Categories">CATEGORIES</NavLink>
+        </li>
+      </ul>
+      <button
+        type="button"
+        className="menu-btn"
+      >
+        <BsFillPersonFill className="menu-btn-icon" />
+      </button>
+    </nav>
   );
 }
 export default Nav;
